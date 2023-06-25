a496c8e9c0f0ea8b4df03dae8246434e87fb697b
Kayenat Patil <89879994+kpatil-7@users.noreply.github.com>
2023-06-25 14:41:44 -0500

    Delete Game: Businessman Simulator directory

--- Game: Businessman Simulator/action.py
+++
@@ -1,8 +0,0 @@
-# This class describe the effect
-
-class Action:
-    def __init__(self, text, effect):
-        # Description of the action
-        self.text = text
-        # How much money the company starts
-        self.effect = effect
